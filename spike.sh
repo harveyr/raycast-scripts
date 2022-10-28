f986bc3bde40ff2bb059806426db98d735e908ea
Harvey Rogers <harveyr@gmail.com>
2022-10-28 14:10:52 -0700

    remove spike.sh

--- spike.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-# osascript -e 'tell application "Google Chrome"' -e 'execute front window\'s active tab javascript "document.title;"' -e 'end tell'
-
-osascript lib/print-chrome-title
