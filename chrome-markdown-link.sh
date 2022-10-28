e5a2f2176aea336e443c73274d56759dd2e2dd2c
Harvey Rogers <harveyr@gmail.com>
2022-10-28 14:13:38 -0700

    comment with link to SO

--- chrome-markdown-link.sh
+++ chrome-markdown-link.sh
@@ -20,6 +20,7 @@ url=$(osascript -e 'tell application "Google Chrome" to get URL of active tab of
 # https://unix.stackexchange.com/questions/103252/how-do-i-get-a-websites-title-using-command-line
 # title=$(wget -qO- "$url" | perl -l -0777 -ne 'print $1 if /<title.*?>\s*(.*?)\s*<\/title/si')
 
+# https://stackoverflow.com/questions/5135609/can-applescript-access-browser-tabs-and-execute-javascript-in-them
 title=$(osascript lib/print-chrome-title)
 
 echo "[$title]($url)" | pbcopy
