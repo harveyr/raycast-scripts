53772f948d170b01046874664c3c498235e60b52
Harvey Rogers <harveyr@gmail.com>
2023-02-10 08:55:52 -0800

    new: activate daily log in chrome

---
+++ chrome-activate-notes-tab.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Activate Daily Log Doc
+# @raycast.mode silent
+# @raycast.packageName Google Chrome
+#
+# Optional parameters:
+# @raycast.icon ✍️
+#
+# Documentation:
+# @raycast.description Activates Chrome and selects tab #3, where I keep my daily log.
+# @raycast.author Harvey Rogers
+# @raycast.authorURL https://github.com/harveyr
+
+osascript lib/chrome-activate-notes
