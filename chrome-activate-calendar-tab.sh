30a83079f808955f606622329aeedf623c751149
Harvey Rogers <harveyr@gmail.com>
2023-03-07 08:24:36 -0800

    add gmail, calendar comands

---
+++ chrome-activate-calendar-tab.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Switch to Google Calendar
+# @raycast.mode silent
+# @raycast.packageName Google Chrome
+#
+# Optional parameters:
+# @raycast.icon 🗓️
+#
+# Documentation:
+# @raycast.author Harvey Rogers
+# @raycast.authorURL https://github.com/harveyr
+
+osascript lib/chrome-tab-2
---
+++ chrome-activate-gmail-tab.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Switch to Gmail
+# @raycast.mode silent
+# @raycast.packageName Google Chrome
+#
+# Optional parameters:
+# @raycast.icon 📧
+#
+# Documentation:
+# @raycast.author Harvey Rogers
+# @raycast.authorURL https://github.com/harveyr
+
+osascript lib/chrome-tab-1
